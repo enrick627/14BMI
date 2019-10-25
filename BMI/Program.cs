2687a1ca5040a59cb0e950b7f79c1c23a97ff8bc
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2019-10-25 11:37:03 +0200

    Add project files.

---
+++ BMI/Program.cs
@@ -0,0 +1,45 @@
+﻿//Enrick De Munter 5ITN nr4
+/*analyse
+ * 
+ * vraag gewicht
+ * vraag lengte
+ * 
+ * Bereken
+ * BMI( gewicht /(lengte * lengte)
+ * 
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Declaraties
+            double gewicht, lengte;
+            double BMI;
+
+            //input
+            //vraag gewicht
+            Console.WriteLine("wat is u gewicht in kilo's?: ");
+            gewicht = double.Parse(Console.ReadLine());
+
+            //vraag lengte
+            Console.WriteLine("wat is uw lengte in meters?: ");
+            lengte = 
+
+
+
+            //wachten
+            Console.WriteLine();
+            Console.WriteLine("druk op enter om af te sluiten");
+            Console.ReadKey();
+        }
+    }
+}
